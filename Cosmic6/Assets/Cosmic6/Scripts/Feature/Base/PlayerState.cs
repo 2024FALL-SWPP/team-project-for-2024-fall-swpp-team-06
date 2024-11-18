c223e0a59c475e35f66b16847a7d987b4a5db4a6
kimyung-kim <kkm7956@snu.ac.kr>
2024-11-18 23:01:11 +0900

    State 3 changed + base click added
    added water, base click function.

---
+++ Cosmic6/Assets/Cosmic6/Scripts/Feature/Base/PlayerState.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public bool isRegistered1 = false;
+    public bool isRegistered2 = false;
+    public bool isRegistered3 = false;
+    
+}
