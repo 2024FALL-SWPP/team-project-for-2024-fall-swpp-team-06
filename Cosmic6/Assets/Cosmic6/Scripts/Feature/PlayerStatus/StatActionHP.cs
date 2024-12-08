edcee6a76d3a496c1e823b39aa7e8cf068189f24
yuzzznk <yujin.k@snu.ac.kr>
2024-12-09 02:11:54 +0900

    얘는 어쩔 수 없는거같다 -> NullReferenceException: Object reference not set to an instance of an object UnityEditor.Graphs.Edge.WakeUp () (at <10c677b3d79d4311829fabebfc141bcd>:0) UnityEditor.Graphs.Graph.DoWakeUpEdges (System.Collections.Generic.List`1[T] inEdges, System.Collections.Generic.List`1[T] ok, System.Collections.Generic.List`1[T] error, System.Boolean inEdgesUsedToBeValid) (at <10c677b3d79d4311829fabebfc141bcd>:0) UnityEditor.Graphs.Graph.WakeUpEdges (System.Boolean clearSlotEdges) (at <10c677b3d79d4311829fabebfc141bcd>:0) UnityEditor.Graphs.Graph.WakeUp (System.Boolean force) (at <10c677b3d79d4311829fabebfc141bcd>:0) UnityEditor.Graphs.Graph.WakeUp () (at <10c677b3d79d4311829fabebfc141bcd>:0) UnityEditor.Graphs.Graph.OnEnable () (at <10c677b3d79d4311829fabebfc141bcd>:0)

--- Cosmic6/Assets/Cosmic6/Scripts/Feature/PlayerStatus/StatActionHP.cs
+++ Cosmic6/Assets/Cosmic6/Scripts/Feature/PlayerStatus/StatActionHP.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DevionGames.InventorySystem.ItemActions;
 
 namespace DevionGames.InventorySystem.ItemActions
 {
