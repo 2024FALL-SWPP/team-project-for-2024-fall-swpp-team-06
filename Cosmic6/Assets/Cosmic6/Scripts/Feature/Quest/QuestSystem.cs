6b955426eb6a513ea58ebd378f0107ede187752e
kimyung-kim <kkm7956@snu.ac.kr>
2024-12-15 23:32:57 +0900

    Revert "Fix Camera + Quest Popup"
    This reverts commit 6b2e57252987e3ef0fc50eafe187c5b9341ff79a.

--- Cosmic6/Assets/Cosmic6/Scripts/Feature/Quest/QuestSystem.cs
+++ Cosmic6/Assets/Cosmic6/Scripts/Feature/Quest/QuestSystem.cs
@@ -14,10 +14,6 @@ public class QuestSystem : MonoBehaviour
 
     public GameObject popupPrefab;
     public Transform popupParent;
-    public float popupDuration = 5f;
-    public float verticalSpacing = 60f;
-
-    private List<GameObject> activePopups = new List<GameObject>();
 
     private Dictionary<string, List<Quest>> baseQuests = new Dictionary<string, List<Quest>>()
     {
@@ -216,36 +212,9 @@ public class QuestSystem : MonoBehaviour
             titleText.text = quest.title;
             contentText.text = quest.description;
 
-            
-
             popupInstance.name = $"Popup_{quest.title}";
 
-            activePopups.Add(popupInstance);
-            UpdatePopupPositions();
-            StartCoroutine(RemovePopupAfterDelay(popupInstance));
-    
-        }
-    }
-
-    private void UpdatePopupPositions()
-    {
-        for (int i = 0; i < activePopups.Count; i++)
-        {
-            RectTransform rectTransform = activePopups[i].GetComponent<RectTransform>();
-            rectTransform.anchoredPosition = new Vector2(0, -i * verticalSpacing);
-        }
-    }
-
-    private IEnumerator RemovePopupAfterDelay(GameObject popupInstance)
-    {
-        yield return new WaitForSeconds(popupDuration);
-
-        if (activePopups.Contains(popupInstance))
-        {
-            activePopups.Remove(popupInstance);
-            Destroy(popupInstance);
-
-            UpdatePopupPositions();
+            Destroy(popupInstance, 5f);
         }
     }
 }
