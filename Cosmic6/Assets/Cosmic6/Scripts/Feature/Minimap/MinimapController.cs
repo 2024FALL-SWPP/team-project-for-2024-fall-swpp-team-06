83494f1e4ee40cef2b9f75f27942ea2cd255ebd2
yuzzznk <yujin.k@snu.ac.kr>
2024-12-16 20:03:45 +0900

    commit to resolve merge conflict

--- Cosmic6/Assets/Cosmic6/Scripts/Feature/Minimap/MinimapController.cs
+++ Cosmic6/Assets/Cosmic6/Scripts/Feature/Minimap/MinimapController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MinimapController : MonoBehaviour
 {
@@ -11,28 +12,54 @@ public class MinimapController : MonoBehaviour
 
     public SpriteRenderer playerIcon;
     public SpriteRenderer[] baseIcons;
-    public RawImage minimapRawImage;
-    public Transform playerTransform;
-    public BaseManager baseManager;
+    //public SpriteRenderer[] regionSprites;
 
+    //public PlayerState playerState;
+
+    public Transform playerTransform;
+    
+    private RawImage rawImage;
+
+    // Start is called before the first frame update
     void Start()
     {
+        rawImage = GetComponent<RawImage>();
         if (fullmapCamera != null)
+        {
             fullmapCamera.gameObject.SetActive(false);
-
-        foreach (var icon in baseIcons)
-            icon.gameObject.SetActive(false);
+        }
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (playerTransform != null)
-            playerIcon.transform.position = playerIcon.transform.parent.position;
+        
+        //UpdateRegionVisibility();
+        
+        minimapCamera.transform.position = 
+            new (playerTransform.position.x, minimapCamera.transform.position.y, playerTransform.position.z);
 
-        if (isFullMap)
-            playerIcon.transform.rotation = Quaternion.Euler(90, 30, 30);
-        else
-            playerIcon.transform.localRotation = Quaternion.Euler(90, 30, 120);
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (isFullMap)
+            {
+                CloseFullMap();
+            }
+            else
+            {
+                ShowFullMap();
+            }
+            /*RaycastHit hit;
+            Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("Minimap"))
+                {
+                    ShowFullMap();
+                }
+            }*/
+        }
 
         if (Input.GetKeyDown(KeyCode.Escape) && isFullMap)
         {
@@ -40,88 +67,43 @@ public class MinimapController : MonoBehaviour
         }
     }
 
-    public void ShowFullMap()
+    /*void UpdateRegionVisibility()
     {
-        if (fullmapCamera != null)
-        {
-            minimapCamera.gameObject.SetActive(false);
-            fullmapCamera.gameObject.SetActive(true);
-        }
+        if (playerState == null || regionSprites == null || regionSprites.Length < 3) return;
 
-        if (minimapRawImage != null)
-        {
-            minimapRawImage.color = new Color(
-                minimapRawImage.color.r,
-                minimapRawImage.color.g,
-                minimapRawImage.color.b,
-                0f
-            );
-        }
+        regionSprites[0].enabled = playerState.isRegistered1;   // Region1
+        regionSprites[1].enabled = playerState.isRegistered2;   // Region2
+        regionSprites[2].enabled = playerState.isRegistered3;   // Region3
+    }*/
 
+    void ShowFullMap()
+    {
+        
+        minimapCamera.gameObject.SetActive(false);
+        fullmapCamera.gameObject.SetActive(true);
+        rawImage.enabled = false;
+        
         isFullMap = true;
-        playerIcon.transform.localScale = new Vector3(200, 200, 1);
-        SetBaseIconScale(new Vector3(50, 50, 1));
+
+        playerIcon.transform.localScale = new Vector3(150, 150, 1);
+        foreach (var icon in baseIcons)
+        {
+            icon.transform.localScale = new Vector3(10, 10, 1);
+        }
     }
 
     public void CloseFullMap()
     {
-        if (fullmapCamera != null)
-        {
-            fullmapCamera.gameObject.SetActive(false);
-            minimapCamera.gameObject.SetActive(true);
-        }
-
-        if (minimapRawImage != null)
-        {
-            minimapRawImage.color = new Color(
-                minimapRawImage.color.r,
-                minimapRawImage.color.g,
-                minimapRawImage.color.b,
-                1f
-            );
-        }
-
+        fullmapCamera.gameObject.SetActive(false);
+        minimapCamera.gameObject.SetActive(true);
+        rawImage.enabled = true;
+        
         isFullMap = false;
-        playerIcon.transform.localScale = new Vector3(5, 5, 1);
-        SetBaseIconScale(new Vector3(3, 3, 1));
-    }
 
-    public bool IsFullMap()
-    {
-        return isFullMap;
-    }
-
-    private void SetBaseIconScale(Vector3 desiredWorldScale)
-    {
+        playerIcon.transform.localScale = new Vector3(12.5f, 12.5f, 1);
         foreach (var icon in baseIcons)
         {
-            Vector3 parentScale = icon.transform.parent != null
-                ? icon.transform.parent.localScale
-                : Vector3.one;
-
-            Vector3 correctedLocalScale = new Vector3(
-                desiredWorldScale.x / parentScale.x,
-                desiredWorldScale.y / parentScale.y,
-                desiredWorldScale.z / parentScale.z
-            );
-
-            icon.transform.localScale = correctedLocalScale;
-        }
-    }
-
-    public void UpdateMinimap()
-    {
-        for (int i = 0; i < baseManager.isBaseRegistered.Length; i++)
-        {
-            if (baseManager.isBaseRegistered[i])
-            {
-                if (i == 0)
-                    baseIcons[0].gameObject.SetActive(true);
-                else if (i == 1)
-                    baseIcons[1].gameObject.SetActive(true);
-                else
-                    baseIcons[2].gameObject.SetActive(true);
-            }
+            icon.transform.localScale = new Vector3(1, 1, 1);
         }
     }
 }
